8fe4cec8fbd976595a280f6405b9c1680cfaa6ce
projectCHOI <yoonsuk.choi93@gmail.com>
2024-11-11 11:11:28 +0900

    js

--- JS/1_학습/241108_17_실습-이벤트-4.js
+++ JS/1_학습/241108_17_실습-이벤트-4.js
@@ -1,1 +1,30 @@
-// 방명록 등록하기
+let writer = document.querySelector('#writer');
+let content = document.querySelector('#content');
+console.log(writer, content);
+
+// [등록] 버튼 클릭시 writeNote() 함수가 실행되도록 인라인 방식으로 미리 설정해 두었습니다.
+function writeNote() {
+  //작성자나 내용이 빈 경우 경고하고 함수 종료
+  if (writer.value === '' || content.value === '') {
+    alert('작성자와 내용을 모두 입력해주세요 🙂');
+    return;
+  }
+
+  const tr = document.createElement('tr');
+
+  const writerTd = document.createElement('td');
+  writerTd.textContent = writer.value;
+
+  const contentTd = document.createElement('td');
+  contentTd.textContent = content.value;
+
+  const dateTd = document.createElement('td');
+  const today = new Date();
+  dateTd.textContent = today.toLocaleDateString();
+  
+  tr.append(writerTd, contentTd, dateTd);
+  document.querySelector('#table').append(tr);
+
+  writer.value = '';
+  content.value = '';
+}
