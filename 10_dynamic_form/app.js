4f4efd5319f0473bfa25edf966379f95e9899acf
projectCHOI <yoonsuk.choi93@gmail.com>
2024-11-25 11:28:04 +0900

    api

--- 10_dynamic_form/app.js
+++ 10_dynamic_form/app.js
@@ -15,30 +15,58 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-/*--------------------------*/
+/* ajax 요청 */
+// /ajax GET
 app.get("/ajax", (req, res) => {
-  const { name, gender } = req.query; // GET 요청의 데이터 추출
-  res.json({ name, gender }); // JSON 응답
+  console.log(req.query);
+  //   res.send("응답!");
+  res.send(req.query);
 });
 
+// /ajax POST
 app.post("/ajax", (req, res) => {
-  const { name, gender } = req.body; // POST 요청의 데이터 추출
-  res.json({ name, gender }); // JSON 응답
+  // body-parser 설정을 해야 req.body를 읽을 수 있음
+  console.log(req.body);
+  res.send(req.body);
 });
 
+/* axios 요청 */
+// /axios GET
 app.get("/axios", (req, res) => {
-  const { name, gender } = req.query;
-  res.json({ name, gender });
+  console.log(req.query);
+  res.send(req.query);
+  /* 
+  {name:'allie', gender:'여성'}
+  */
 });
 
+// /axios POST
 app.post("/axios", (req, res) => {
-  const { name, gender } = req.body;
-  res.json({ name, gender });
+  console.log(req.body);
+  res.send(req.body);
 });
 
-/*?*/
+/* fetch 요청 */
+app.get("/fetch", (req, res) => {
+  console.log(req.query);
+  // 클라이언트에서 .text() 사용
+  //   res.send("응답완료!!");
 
-// 가장 아래 위치
+  // 클라이언트에서 .json() 사용
+  res.send(req.query); // 객체를 보낸다.
+});
+
+app.post("/fetch", (req, res) => {
+  console.log(req.body); // 객체
+  //{ name: 'allie', gender: '여성' }
+  res.send(req.body);
+});
+
+// 외부 api 사용하기
+app.get("/api", (req, res) => {
+  res.render("api");
+});
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
