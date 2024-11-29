4451070e95ee4bae8883527459e6c9a18eb05ed5
projectCHOI <yoonsuk.choi93@gmail.com>
2024-11-29 16:11:11 +0900

    MVC

--- 14_MVC/app.js
+++ 14_MVC/app.js
@@ -7,47 +7,17 @@ app.set("views", "./views");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const comments = [
-  {
-    id: 1,
-    userid: "apple",
-    date: "2024-10-23",
-    comment: "안녕하세요~~~~~~~~~~~~",
-  },
-  {
-    id: 2,
-    userid: "banana",
-    date: "2024-11-23",
-    comment: "반갑습니다!!!!!~~~~~",
-  },
-  {
-    id: 3,
-    userid: "cocoa",
-    date: "2024-09-23",
-    comment: "아무거나 쓰겠습니다...",
-  },
-  {
-    id: 4,
-    userid: "donut",
-    date: "2024-10-31",
-    comment: "곧 점심시간 이네요..",
-  },
-];
+// 라우터 분리
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-// comment로 이동
-app.get("/comments", (req, res) => {
-  console.log(comments);
-  res.render("comments", { commentInfos: comments });
-});
-// comment에서 id로
-app.get("/comment/:id", (req, res) => {
-  //   console.log(req.params);
-  //   console.log(req.query);
-  const commentId = req.params.id;
-  res.render("comment");
+const userRouter = require("./routes/user");
+app.use("/user", userRouter);
+
+// [404 error]
+// 반드시 맨 마지막 라우트로 선언
+app.get("*", (req, res) => {
+  res.render("404");
 });
 
 app.listen(PORT, () => {
