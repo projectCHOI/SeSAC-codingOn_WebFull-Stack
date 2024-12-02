83bc76809ec7b5cc903ebe6a0aaf0f7879757e7f
projectCHOI <yoonsuk.choi93@gmail.com>
2024-12-02 16:07:19 +0900

    sql

--- 13_MySQL2/SQL.sql
+++ 13_MySQL2/SQL.sql
@@ -1,16 +1,14 @@
 -- Active: 1733117497760@@127.0.0.1@3306@sesac
-############### SQL 연결 확인 ###############
+############### SQL 연결 ###############
 SHOW DATABASES;
 USE sesac;
 SHOW TABLES;
-############### sesac; 연결 확인 ###############
-
 ############### DCL ###############
--- MySQL 사용자 생성
 CREATE USER 'sesac'@'%' IDENTIFIED BY '1234';
--- 권한 부여
-GRANT ALL PRIVILEGES ON *.* TO 'sesac'@'%' WITH GRANT OPTION;
+SELECT User, Host FROM mysql.user WHERE User = 'sesac';
+DROP USER 'sesac'@'%';
+CREATE USER 'sesac'@'%' IDENTIFIED BY '1234';
+GRANT SELECT, INSERT, UPDATE, DELETE ON sesac.* TO 'sesac'@'%';
 ALTER USER 'sesac'@'%' IDENTIFIED WITH mysql_native_password BY '1234';
 FLUSH PRIVILEGES;
-SELECT * FROM mysql.user;
-show GRANTS for 'sesac'@'%';
+SHOW GRANTS FOR 'sesac'@'%';
