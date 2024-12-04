72211cc1f023a94498f32810b73c928edad94af3
projectCHOI <yoonsuk.choi93@gmail.com>
2024-12-04 16:40:25 +0900

    MVC3

--- 14_MVC3_MySQL_release/app.js
+++ 14_MVC3_MySQL_release/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.set('view engine', 'ejs');
-app.set('views', './views'); 
-app.use('/static', express.static(__dirname + '/static'));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/static", express.static(__dirname + "/static"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
