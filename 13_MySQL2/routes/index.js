22773dd7968a96ff3cc99945688caebb0b8c93ec
projectCHOI <yoonsuk.choi93@gmail.com>
2024-12-04 12:57:02 +0900

    mysql

--- 13_MySQL2/routes/index.js
+++ 13_MySQL2/routes/index.js
@@ -4,5 +4,10 @@ const controller = require("../controller/Cvisitor");
 const router = express.Router();
 
 router.get("/", controller.main);
-router.get("/visitors", controller.getVistors);
+router.get("/visitors", controller.getVisitors);
+router.get("/visitor/:id", controller.getVisitor);
+
+router.post("/visitor", controller.postVisitor);
+router.delete("/visitor", controller.deleteVisitor);
+router.patch("/visitor", controller.patchVisitor);
 module.exports = router;
