feec58ce55ad19bd0c015b8811d7aff96f929d95
projectCHOI <yoonsuk.choi93@gmail.com>
2024-12-04 15:37:13 +0900

    ENV

--- 99_ENV/app.js
+++ 99_ENV/app.js
@@ -1,10 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config(); // 반드시 최상단에 위치해야 환경변수 로드가 가능합니다.
+
 const express = require("express");
 const app = express();
-console.log("db pw", process.env.DB_PASSWORD); // config()위에서는 env정보를 읽을 수 없다.
-// console.log(process.env);
-const dotenv = require("dotenv");
-dotenv.config();
+
+console.log("db pw", process.env.DB_PASSWORD); // 이제 환경변수를 제대로 읽습니다.
 const PORT = process.env.PORT;
+
 console.log("PORT number", PORT);
 console.log("db name", process.env.DB_DATABSE);
 
