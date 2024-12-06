63e8fd4110d8c17d1949f47127b7fa5849ca4b5f
projectCHOI <yoonsuk.choi93@gmail.com>
2024-12-06 14:09:39 +0900

    mvc3

--- 14_MVC3_MySQL_release/app.js
+++ 14_MVC3_MySQL_release/app.js
@@ -2,17 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+// 라우터 파일 불러오기
+const userRouter = require("./routes/user");
+
+// EJS 설정
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+// 정적 파일 경로 설정
 app.use("/static", express.static(__dirname + "/static"));
+
+// body-parser 기능 (JSON 데이터 및 URL-encoded 데이터 파싱)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// TODO: 라우팅 분리
-// 기본 주소: localhost:PORT/user <- 주의!!
+// 라우팅 설정
+// 기본 경로: localhost:PORT/user
+app.use("/user", userRouter);
 
-// TODO: 404 에러 처리
+// 404 에러 처리
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404 Error" });
+});
 
+// 서버 실행
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}/user`);
 });
