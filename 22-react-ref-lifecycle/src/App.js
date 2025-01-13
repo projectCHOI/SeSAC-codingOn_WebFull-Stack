509eb6a21826e13bac3d81f01d4b97fe3a90b1b9
projectCHOI <yoonsuk.choi93@gmail.com>
2025-01-13 10:47:08 +0900

    22-react-Original

--- 22-react-ref-lifecycle/src/App.js
+++ 22-react-ref-lifecycle/src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>다음시간부터</h1>
+      <h2>ref를 해보겠습니다!</h2>
     </div>
   );
 }
