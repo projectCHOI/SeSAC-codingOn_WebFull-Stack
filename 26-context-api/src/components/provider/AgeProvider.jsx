5a117fca37b518166c87fcc2203725e7b7da9a71
projectCHOI <yoonsuk.choi93@gmail.com>
2025-01-23 00:15:07 +0900

    context-api

--- 26-context-api/src/components/provider/AgeProvider.jsx
+++ 26-context-api/src/components/provider/AgeProvider.jsx
@@ -1,1 +1,12 @@
-//
+import { useState } from "react";
+import { AgeContext } from "../../context/AgeContext";
+
+export default function AgeProvider({ children }) {
+  const [age, setAge] = useState(0);
+
+  return (
+    <AgeContext.Provider value={{ age, setAge }}>
+      {children}
+    </AgeContext.Provider>
+  );
+}
---
+++ 26-context-api/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+const defaultUser = {
+  name: "홍길동",
+  setName: () => {},
+};
+export const UserContext = createContext(defaultUser);
