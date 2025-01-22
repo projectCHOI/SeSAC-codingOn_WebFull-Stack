3a723d381b617fadf3c77eebaccf92175fcba50a
projectCHOI <yoonsuk.choi93@gmail.com>
2025-01-22 17:32:12 +0900

    react-routing

---
+++ 25-react-routing/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h2>404 ERROR!!🚨</h2>
+      <Link to="/">Home 으로 이동하기</Link>
+    </div>
+  );
+}
